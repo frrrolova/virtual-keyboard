029c7ec1cf5dc6b50b9b1d40bf4478283e877194
t_frrolova <tatianaaleks.frolova@gmail.com>
2023-04-30 02:47:49 +0200

    feat: add eng keys layout

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const Keyboard = {
   textField: null,
   caps: false,
   engKeysLayout: [[
+    { symbol: '`', size: 'small', keyCode: 192, keyElement: null },
+    { symbol: '1', size: 'small', keyCode: 49, keyElement: null },
+    { symbol: '2', size: 'small', keyCode: 50, keyElement: null },
+    { symbol: '3', size: 'small', keyCode: 51, keyElement: null },
+    { symbol: '4', size: 'small', keyCode: 52, keyElement: null },
+    { symbol: '5', size: 'small', keyCode: 53, keyElement: null },
+    { symbol: '6', size: 'small', keyCode: 54, keyElement: null },
     { symbol: '7', size: 'small', keyCode: 55, keyElement: null },
     { symbol: '8', size: 'small', keyCode: 56, keyElement: null },
     { symbol: '9', size: 'small', keyCode: 57, keyElement: null },
@@ -14,15 +21,53 @@ const Keyboard = {
   ], [
     { symbol: 'Tab', size: 'big', keyCode: 9, keyElement: null },
     { symbol: 'Q', size: 'small', keyCode: 81, keyElement: null },
-    { symbol: 'Del', size: 'big', keyCode: 46, keyElement: null }
+    { symbol: 'W', size: 'small', keyCode: 87, keyElement: null },
+    { symbol: 'E', size: 'small', keyCode: 69, keyElement: null },
+    { symbol: 'R', size: 'small', keyCode: 82, keyElement: null },
+    { symbol: 'T', size: 'small', keyCode: 84, keyElement: null },
+    { symbol: 'Y', size: 'small', keyCode: 89, keyElement: null },
+    { symbol: 'U', size: 'small', keyCode: 85, keyElement: null },
+    { symbol: 'I', size: 'small', keyCode: 54, keyElement: null },
+    { symbol: 'O', size: 'small', keyCode: 73, keyElement: null },
+    { symbol: 'P', size: 'small', keyCode: 79, keyElement: null },
+    { symbol: '[', size: 'small', keyCode: 80, keyElement: null },
+    { symbol: ']', size: 'small', keyCode: 219, keyElement: null },
+    { symbol: '\\', size: 'small', keyCode: 221, keyElement: null },
+    { symbol: 'Del', size: 'big', keyCode: 220, keyElement: null }
   ], [
     { symbol: 'Caps', size: 'big', keyCode: 20, keyElement: null },
+    { symbol: 'A', size: 'small', keyCode: 65, keyElement: null },
+    { symbol: 'S', size: 'small', keyCode: 83, keyElement: null },
+    { symbol: 'D', size: 'small', keyCode: 68, keyElement: null },
+    { symbol: 'F', size: 'small', keyCode: 70, keyElement: null },
+    { symbol: 'G', size: 'small', keyCode: 71, keyElement: null },
+    { symbol: 'H', size: 'small', keyCode: 72, keyElement: null },
+    { symbol: 'J', size: 'small', keyCode: 74, keyElement: null },
+    { symbol: 'K', size: 'small', keyCode: 75, keyElement: null },
+    { symbol: 'L', size: 'small', keyCode: 76, keyElement: null },
+    { symbol: ';', size: 'small', keyCode: 186, keyElement: null },
+    { symbol: `'`, size: 'small', keyCode: 222, keyElement: null },
     { symbol: 'Enter', size: 'big', keyCode: 13, keyElement: null }
   ], [
-    { symbol: 'Shift', size: 'big', keyCode: 16, keyElement: null, side: left },
+    { symbol: 'Shift', size: 'big', keyCode: 16, keyElement: null, side: 'left' },
+    { symbol: 'Z', size: 'small', keyCode: 90, keyElement: null },
+    { symbol: 'X', size: 'small', keyCode: 88, keyElement: null },
+    { symbol: 'C', size: 'small', keyCode: 67, keyElement: null },
+    { symbol: 'V', size: 'small', keyCode: 86, keyElement: null },
+    { symbol: 'B', size: 'small', keyCode: 66, keyElement: null },
+    { symbol: 'N', size: 'small', keyCode: 78, keyElement: null },
+    { symbol: 'M', size: 'small', keyCode: 77, keyElement: null },
+    { symbol: ',', size: 'small', keyCode: 188, keyElement: null },
+    { symbol: '.', size: 'small', keyCode: 190, keyElement: null },
+    { symbol: '/', size: 'small', keyCode: 191, keyElement: null },
     { symbol: '⇧', size: 'small', keyCode: 38, keyElement: null },
+    { symbol: 'Shift', size: 'big', keyCode: 16, keyElement: null, side: 'right' }
   ], [
-    { symbol: 'Ctrl', size: 'big', keyCode: 17, keyElement: null, side: left },
+    { symbol: 'Ctrl', size: 'big', keyCode: 17, keyElement: null, side: 'left' },
+    { symbol: 'Alt', size: 'big', keyCode: 18, keyElement: null, side: 'left' },
+    { symbol: ' ', size: 'extra-big', keyCode: 32, keyElement: null },
+    { symbol: 'Alt', size: 'big', keyCode: 18, keyElement: null, side: 'right' },
+    { symbol: 'Ctrl', size: 'big', keyCode: 17, keyElement: null, side: 'right' },
     { symbol: '⇦', size: 'small', keyCode: 37, keyElement: null },
     { symbol:  '⇩', size: 'small', keyCode: 40, keyElement: null },
     { symbol:  '⇨', size: 'small', keyCode: 39, keyElement: null }
@@ -152,13 +197,19 @@ const Keyboard = {
 
 
             }
-            // this.textField.focus();
           }
 
         )
 
         key.addEventListener(
           'mouseup',
+          (event) => {
+            letter.keyElement.classList.remove("keyboard__key_pressed");
+          }
+        )
+
+        key.addEventListener(
+          'mouseout',
           (event) => {
             letter.keyElement.classList.remove("keyboard__key_pressed");
           }
@@ -174,7 +225,6 @@ const Keyboard = {
     window.addEventListener(
       'keydown',
       (event) => {
-        // this.textField.focus();
         this.engKeysLayout.forEach((item) => {
           const pressedKey = item.find((letter) => event.keyCode === letter.keyCode);
             pressedKey?.keyElement.classList.add("keyboard__key_pressed"); //добавит класс, если есть pressedKey
